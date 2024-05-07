4c57f078a8e8b0725ba3d044c801904c0d66ce38
Fernan2908 <fernando.sierra3@upr.edu>
2024-05-07 16:59:51 -0400

    Update CSS styles for Indicadores page and add graph functionality

--- web_app/views.py
+++ web_app/views.py
@@ -1,12 +1,11 @@
-from os import login_tty
 import pandas as pd
 from django.shortcuts import redirect, render
 import plotly.express as px
-from django.http import HttpResponse
-from .forms import CreateUserForm, SignInForm
+from .forms import SignInForm
 from .models import *
 from django.contrib import messages
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login
+import plotly.graph_objects as go
 
 
 def signin(request):        
@@ -43,4 +42,36 @@ def ciclos_economicos(request):
 
 
 def indicadores(request):
-    return render(request, "indicadores.html")
+    fig = px.scatter( x = [2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 
+                        2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024], 
+                y = [14, 55, 44, 13, 29, 20, 45, 39, 29, 10, 50, 60, 39, 36, 49, 18, 49, 50, 69, 18, 13, 
+                    11, 4, 2, 1], 
+                labels={'x':'Time', 'y':'Values'},
+                )
+
+    fig.add_trace(go.Scatter(x=[2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 
+                            2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024], 
+                            y=[14, 55, 44, 13, 29, 20, 45, 39, 29, 10, 50, 60, 39, 36, 49, 18, 49, 50, 69, 18, 13, 
+                                11, 4, 2, 1], 
+                            mode='lines+markers',
+                            name='test',
+                            line=dict(color='firebrick', width=4),
+                            marker=dict(size=15, color='LightSkyBlue')))
+
+    fig.update_layout(
+        autosize=True,
+        width=900,
+        height=500,
+        margin=dict(
+            l=50,
+            r=50,
+            b=50,
+            t=50,
+            pad=4
+        ),
+        paper_bgcolor="#CDDAFF",
+    )
+    indicadores = fig.to_html()
+
+    context = {'indicadores': indicadores}
+    return render(request, "indicadores.html", context)
