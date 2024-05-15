93948f514260cf6755e848c18fea33be1b376d37
Fernan2908 <fernando.sierra3@upr.edu>
2024-05-15 13:43:30 -0400

    Fix the hamburger menu

--- web_app/urls.py
+++ web_app/urls.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from django.urls import include
 from web_app import urls
 from web_app import views
 
 urlpatterns = [
-    path("", views.home, name="home"),
-    path("signin/", views.signin, name="signin"),
-    path("centro-de-datos-macroeconomicos/", views.macro, name="macro"),
-    path("ciclos-economicos/", views.ciclos_economicos, name="ciclos_economicos"),
-    path("indicadores-mensuales/", views.indicadores, name="indicadores"),
+    re_path(r"^$", views.home, name="home"),
+    re_path(r"^signin/$", views.signin, name="signin"),
+    re_path(r"^centro-de-datos-macroeconomicos/$", views.macro, name="macro"),
+    re_path(r"^ciclos-economicos/$", views.ciclos_economicos, name="ciclos_economicos"),
+    re_path(r"^indicadores-mensuales/$", views.indicadores, name="indicadores"),
 ]
