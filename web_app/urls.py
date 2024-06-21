10ca1563cb70cabeea845062f3933c74df4c19cb
Fernan2908 <fernando.sierra3@upr.edu>
2024-06-21 09:16:33 -0400

    Remove unused view function OfHome and corresponding URL pattern

--- web_app/urls.py
+++ web_app/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     re_path(r"^ciclos-economicos/$", views.ciclos_economicos, name="ciclos_economicos"),
     re_path(r"^indicadores-mensuales/$", views.indicadores, name="indicadores"),
     re_path(r"^datos-demograficos/$", views.datos_demograficos, name="datos_demograficos"),
-    re_path(r"^official-home/$", views.OfHome, name="Of_Home"),
     re_path(r"^succesfull_page/$", views.succesfull_page, name="succesfull_page"),
     re_path(r"^JP-304-relacion-de-aportaciones-federales/$", views.JP_304, name="JP-304"),
     re_path(r"^IP-110-agricultura/$", views.IP_110, name="IP-110"),
