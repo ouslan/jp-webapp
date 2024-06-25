9ffd2e19407be3070e095658873bfa0de9a780fa
EdzelOrtiz <edzel.ortiz@upr.edu>
2024-06-25 12:04:22 -0400

    Update form and view for JP-560-63111

--- web_app/views.py
+++ web_app/views.py
@@ -1039,4 +1039,62 @@ def JP_560(request):
 
 
 def JP_560_63111(request):
+
+    if request.method == "POST":
+        # Retrieve form data
+        ssn = request.POST.get('ssn')
+        tel = request.POST.get('tel')
+        fax = request.POST.get('fax')
+        sales_1 = request.POST.get('sales_1')
+        sales_2 = request.POST.get('sales_2')
+        premiums_1 = request.POST.get('premiums_1')
+        premiums_2 = request.POST.get('premiums_2')
+        interest_received_1 = request.POST.get('interest_received_1')
+        interest_received_2 = request.POST.get('interest_received_2')
+        other_income_1 = request.POST.get('other_income_1')
+        other_income_2 = request.POST.get('other_income_2')
+        total_income_1 = request.POST.get('total_income_1')
+        total_income_2 = request.POST.get('total_income_2')
+        interest_paid_1 = request.POST.get('interest_paid_1')
+        interest_paid_2 = request.POST.get('interest_paid_2')
+        claims_paid_1 = request.POST.get('claims_paid_1')
+        claims_paid_2 = request.POST.get('claims_paid_2')
+        other_expenditures_1 = request.POST.get('other_expenditures_1')
+        other_expenditures_2 = request.POST.get('other_expenditures_2')
+        total_expenditures_1 = request.POST.get('total_expenditures_1')
+        total_expenditures_2 = request.POST.get('total_expenditures_2')
+        net_profit_loss_1 = request.POST.get('net_profit_loss_1')
+        net_profit_loss_2 = request.POST.get('net_profit_loss_2')
+        initial_inventory_1 = request.POST.get('initial_inventory_1')
+        initial_inventory_2 = request.POST.get('initial_inventory_2')
+        final_inventory_1 = request.POST.get('final_inventory_1')
+        final_inventory_2 = request.POST.get('final_inventory_2')
+        signature = request.POST.get('signature')
+        rank = request.POST.get('rank')
+
+
+        csv_file_path = 'data/cuestionarios/ingreso_neto/JP-560-63111.csv'
+        file_exists = os.path.isfile(csv_file_path) and os.path.getsize(csv_file_path) > 0
+
+        with open(csv_file_path, mode='a', newline='') as file:
+            writer = csv.writer(file)
+            
+            if not file_exists:
+                writer.writerow(["ssn", "tel", "fax", "sales_1", "sales_2", "premiums_1", "premiums_2", "interest_received_1",
+                                 "interest_received_2", "other_income_1", "other_income_2", "total_income_1", "total_income_2",
+                                "interest_paid_1", "interest_paid_2", "claims_paid_1", "claims_paid_2", "other_expenditures_1",
+                                "other_expenditures_2", "total_expenditures_1", "total_expenditures_2", "net_profit_loss_1",
+                                "net_profit_loss_2", "initial_inventory_1", "initial_inventory_2", "final_inventory_1", "final_inventory_2",
+                                "signature", "rank"])
+                
+            writer.writerow([
+                            ssn, tel, fax, sales_1, sales_2, premiums_1, premiums_2, interest_received_1, interest_received_2,
+                            other_income_1, other_income_2, total_income_1, total_income_2, interest_paid_1, interest_paid_2,
+                            claims_paid_1, claims_paid_2, other_expenditures_1, other_expenditures_2, total_expenditures_1,
+                            total_expenditures_2, net_profit_loss_1, net_profit_loss_2, initial_inventory_1, initial_inventory_2,
+                            final_inventory_1, final_inventory_2, signature, rank
+                            ])
+            
+        return render(request, "cuestionarios/succesfull.html")
+
     return render(request, "cuestionarios/ingreso_neto/JP-560-63111.html")
