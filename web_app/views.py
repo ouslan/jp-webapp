190d53749e5c4e178a0880e46084b696c356fd0a
EdzelOrtiz <edzel.ortiz@upr.edu>
2024-06-26 13:51:42 -0400

    Add JP_560_2 form and view backend part

--- web_app/views.py
+++ web_app/views.py
@@ -1561,4 +1561,65 @@ def JP_560_63210(request):
     return render(request, "cuestionarios/ingreso_neto/JP-560-63210.html")
 
 def JP_560_2(request):
+
+    if request.method == "POST":
+        # Retrieve form data
+        company_name = request.POST.get('company_name')
+        address = request.POST.get('address')
+        email = request.POST.get('email')
+        liaison_officer = request.POST.get('liaison_officer')
+        ssn = request.POST.get('ssn')
+        tel = request.POST.get('tel')
+        fax = request.POST.get('fax')
+        sales_1 = request.POST.get('sales_1')
+        sales_2 = request.POST.get('sales_2')
+        interest_received_1 = request.POST.get('interest_received_1')
+        interest_received_2 = request.POST.get('interest_received_2')
+        other_income_1 = request.POST.get('other_income_1')
+        other_income_2 = request.POST.get('other_income_2')
+        total_income_1 = request.POST.get('total_income_1')
+        total_income_2 = request.POST.get('total_income_2')
+        interest_paid_1 = request.POST.get('interest_paid_1')
+        interest_paid_2 = request.POST.get('interest_paid_2')
+        other_expenditures_1_1 = request.POST.get('other_expenditures_1_1')
+        other_expenditures_2_1 = request.POST.get('other_expenditures_1_2')
+        other_expenditures_1_2 = request.POST.get('other_expenditures_2_1')
+        other_expenditures_2_2 = request.POST.get('other_expenditures_2_2')
+        net_profit_loss_1 = request.POST.get('net_profit_loss_1')
+        net_profit_loss_2 = request.POST.get('net_profit_loss_2')
+        initial_inventory_1 = request.POST.get('initial_inventory_1')
+        initial_inventory_2 = request.POST.get('initial_inventory_2')
+        final_inventory_1 = request.POST.get('final_inventory_1')
+        final_inventory_2 = request.POST.get('final_inventory_2')
+        signature = request.POST.get('signature')
+        rank = request.POST.get('rank')
+
+
+        csv_file_path = 'data/cuestionarios/ingreso_neto/JP-560-2.csv'
+        file_exists = os.path.isfile(csv_file_path) and os.path.getsize(csv_file_path) > 0
+
+        with open(csv_file_path, mode='a', newline='') as file:
+            writer = csv.writer(file)
+            
+            if not file_exists:
+                writer.writerow([
+                                "company_name", "address", "email", "liaison_officer", 
+                                "ssn", "tel", "fax", "sales_1", "sales_2", "interest_received_1", 
+                                "interest_received_2", "other_income_1", "other_income_2", "total_income_1", 
+                                "total_income_2", "interest_paid_1", "interest_paid_2", "other_expenditures_1_1",
+                                "other_expenditures_2_1", "other_expenditures_1_2", "other_expenditures_2_2",
+                                "net_profit_loss_1", "net_profit_loss_2", "initial_inventory_1", "initial_inventory_2",
+                                "final_inventory_1", "final_inventory_2", "signature", "rank"
+                                ])
+                
+            writer.writerow([
+                            company_name, address, email, liaison_officer, ssn, tel, fax, sales_1, sales_2, interest_received_1,
+                            interest_received_2, other_income_1, other_income_2, total_income_1, total_income_2, interest_paid_1,
+                            interest_paid_2, other_expenditures_1_1, other_expenditures_2_1, other_expenditures_1_2,
+                            other_expenditures_2_2, net_profit_loss_1, net_profit_loss_2, initial_inventory_1, initial_inventory_2,
+                            final_inventory_1, final_inventory_2, signature, rank
+                            ])
+            
+        return render(request, "cuestionarios/succesfull.html")
+
     return render(request, "cuestionarios/ingreso_neto/JP-560-2.html")
