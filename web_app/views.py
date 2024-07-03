0fc67d42b670cb5b197a8e48d1077b5e148bce34
Onnelle Lugo <onnelle.lugo@upr.edu>
2024-07-03 10:32:07 -0400

    unit 520 form

--- web_app/views.py
+++ web_app/views.py
@@ -3362,3 +3362,108 @@ def IP_480a(request):
             
         return render(request, "forms/succesfull.html")
     return render(request, "forms/yearly/ingreso_neto/IP-480a.html")
+
+def IP_520(request):
+    if request.method == "POST":
+        # Retrieve form data
+        company_name = request.POST.get('company_name')
+        address = request.POST.get('address')
+        email = request.POST.get('email')
+        liaison_officer = request.POST.get('liaison_officer')
+        
+        ssn = request.POST.get('ssn')
+        tel = request.POST.get('tel')
+        fax = request.POST.get('fax')
+        
+        legal_form = request.POST.get('legal_form')
+        cfc = request.POST.get('cfc')
+        main_line = request.POST.get('main_line')
+        business_type = request.POST.get('business_type')
+        other_business_type = request.POST.get('other_business_type')
+        accounting_period = request.POST.get('accounting_period')
+        start_year = request.POST.get('start_year')
+        end_year = request.POST.get('end_year')
+        incomes_sales_1 = request.POST.get('incomes_sales_1')
+        incomes_sales_2 = request.POST.get('incomes_sales_2')
+        incomes_from_people_1 = request.POST.get('incomes_from_people_1')
+        incomes_from_people_2 = request.POST.get('incomes_from_people_2')
+        incomes_industries_businesses_1 = request.POST.get('incomes_industries_businesses_1')
+        incomes_industries_businesses_2 = request.POST.get('incomes_industries_businesses_2')
+        incomes_less_cost_1 = request.POST.get('incomes_less_cost_1')
+        incomes_less_cost_2 = request.POST.get('incomes_less_cost_2')
+        incomes_inventory_beginning_1 = request.POST.get('incomes_inventory_beginning_1')
+        incomes_inventory_beginning_2 = request.POST.get('incomes_inventory_beginning_2')
+        incomes_purchases_1 = request.POST.get('incomes_purchases_1')
+        incomes_purchases_2 = request.POST.get('incomes_purchases_2')
+        incomes_inventory_end_1 = request.POST.get('incomes_inventory_end_1')
+        incomes_inventory_end_2 = request.POST.get('incomes_inventory_end_2')
+        incomes_gross_profit_1 = request.POST.get('incomes_gross_profit_1')
+        incomes_gross_profit_2 = request.POST.get('incomes_gross_profit_2')
+        other_operation_incomes_1 = request.POST.get('other_operation_incomes_1')
+        other_operation_incomes_2 = request.POST.get('other_operation_incomes_2')
+        incomes_interests_1 = request.POST.get('incomes_interests_1')
+        incomes_interests_2 = request.POST.get('incomes_interests_2')
+        incomes_rent_1 = request.POST.get('incomes_rent_1')
+        incomes_rent_2 = request.POST.get('incomes_rent_2')
+        incomes_gain_loss_1 = request.POST.get('incomes_gain_loss_1')
+        incomes_gain_loss_2 = request.POST.get('incomes_gain_loss_2')
+        incomes_dividends_1 = request.POST.get('incomes_dividends_1')
+        incomes_dividends_2 = request.POST.get('incomes_dividends_2')
+        other_incomes_1 = request.POST.get('other_incomes_1')
+        other_incomes_2 = request.POST.get('other_incomes_2')
+        incomes_total_gross_1 = request.POST.get('incomes_total_gross_1')
+        incomes_total_gross_2 = request.POST.get('incomes_total_gross_2')
+        expenses_1 = request.POST.get('expenses_1')
+        expenses_2 = request.POST.get('expenses_2')
+        expenses_salaries_wages_bonus_1 = request.POST.get('expenses_salaries_wages_bonus_1')
+        expenses_salaries_wages_bonus_2 = request.POST.get('expenses_salaries_wages_bonus_2')
+        expenses_interests_1 = request.POST.get('expenses_interests_1')
+        expenses_interests_2 = request.POST.get('expenses_interests_2')
+        expenses_depreciation_1 = request.POST.get('expenses_depreciation_1')
+        expenses_depreciation_2 = request.POST.get('expenses_depreciation_2')
+        expenses_rent_1 = request.POST.get('expenses_rent_1')
+        expenses_rent_2 = request.POST.get('expenses_rent_2')
+        expenses_bad_debts_1 = request.POST.get('expenses_bad_debts_1')
+        expenses_bad_debts_2 = request.POST.get('expenses_bad_debts_2')
+        expenses_donations_1 = request.POST.get('expenses_donations_1')
+        expenses_donations_2 = request.POST.get('expenses_donations_2')
+        expenses_sales_taxes_1 = request.POST.get('expenses_sales_taxes_1')
+        expenses_sales_taxes_2 = request.POST.get('expenses_sales_taxes_2')
+        expenses_machinary_1 = request.POST.get('expenses_machinary_1')
+        expenses_machinary_2 = request.POST.get('expenses_machinary_2')
+        expenses_other_purchases_1 = request.POST.get('expenses_other_purchases_1')
+        expenses_other_purchases_2 = request.POST.get('expenses_other_purchases_2')
+        expenses_licenses_1 = request.POST.get('expenses_licenses_1')
+        expenses_licenses_2 = request.POST.get('expenses_licenses_2')
+        expenses_other_operations_1 = request.POST.get('expenses_other_operations_1')
+        expenses_other_operations_2 = request.POST.get('expenses_other_operations_2')
+        expenses_total_operations_1 = request.POST.get('expenses_total_operations_1')
+        expenses_total_operations_2 = request.POST.get('expenses_total_operations_2')
+        gross_profit_1 = request.POST.get('gross_profit_1')
+        gross_profit_2 = request.POST.get('gross_profit_2')
+        profit_income_tax_1 = request.POST.get('profit_income_tax_1')
+        profit_income_tax_2 = request.POST.get('profit_income_tax_2')
+        profit_after_income_tax_1 = request.POST.get('profit_after_income_tax_1')
+        profit_after_income_tax_2 = request.POST.get('profit_after_income_tax_2')
+        sales_tax_1 = request.POST.get('sales_tax_1')
+        sales_tax_2 = request.POST.get('sales_tax_2')
+        name = request.POST.get('name')
+        rank = request.POST.get('rank')
+        
+        csv_file_path = 'data/cuestionarios/ingreso_neto/IP-520.csv'
+        file_exists = os.path.isfile(csv_file_path) and os.path.getsize(csv_file_path) > 0
+
+        with open(csv_file_path, mode='a', newline='') as file:
+            writer = csv.writer(file)
+            
+            if not file_exists:
+                writer.writerow([
+
+                                ])
+            
+            writer.writerow([
+
+                            ])  
+
+        return render(request, "forms/succesfull.html")
+    return render(request, "forms/yearly/ingreso_neto/IP-520.html")
