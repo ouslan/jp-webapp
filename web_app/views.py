f1c2f886b82ffbc30e63a972e0e0d95d2efec5bd
EdzelOrtiz <edzel.ortiz@upr.edu>
2024-07-03 10:28:33 -0400

    Update labels and input names in IP-490.html form

--- web_app/views.py
+++ web_app/views.py
@@ -3384,4 +3384,108 @@ def IP_480a(request):
     return render(request, "forms/yearly/ingreso_neto/IP-480a.html")
 
 def IP_490(request):
+
+    if request.method == "POST":
+        # Retrieve form data
+        company_name = request.POST.get('company_name')
+        address = request.POST.get('address')
+        email = request.POST.get('email')
+        liaison_officer = request.POST.get('liaison_officer')
+        ssn = request.POST.get('ssn')
+        tel = request.POST.get('tel')
+        fax = request.POST.get('fax')
+        legal_form = request.POST.get('legal_form')
+        cfc = request.POST.get('cfc')
+        business_type = request.POST.get('business_type')
+        business_function = request.POST.get('business_function')
+        closing_date = request.POST.get('closing_date')
+        start_year = request.POST.get('start_year')
+        end_year = request.POST.get('end_year')
+        income_operations_1 = request.POST.get('income_operations_1')
+        income_operations_2 = request.POST.get('income_operations_2')
+        people_A_1 = request.POST.get('people_A_1')
+        people_A_2 = request.POST.get('people_A_2')
+        industries_businesses_A_1 = request.POST.get('industries_businesses_A_1')
+        industries_businesses_A_2 = request.POST.get('industries_businesses_A_2')
+        interest_A_1 = request.POST.get('interest_A_1')
+        interest_A_2 = request.POST.get('interest_A_2')
+        rent_land_1 = request.POST.get('rent_land_1')
+        rent_land_2 = request.POST.get('rent_land_2')
+        capital_gain_1 = request.POST.get('capital_gain_1')
+        capital_gain_2 = request.POST.get('capital_gain_2')
+        other_income_A_1 = request.POST.get('other_income_A_1')
+        other_income_A_2 = request.POST.get('other_income_A_2')
+        total_income_A_1 = request.POST.get('total_income_A_1')
+        total_income_A_2 = request.POST.get('total_income_A_2')
+        salaries_wages_bonus_B_1 = request.POST.get('salaries_wages_bonus_B_1')
+        salaries_wages_bonus_B_2 = request.POST.get('salaries_wages_bonus_B_2')
+        interests_B_1 = request.POST.get('interests_B_1')
+        interests_B_2 = request.POST.get('interests_B_2')
+        rent_B_1 = request.POST.get('rent_B_1')
+        rent_B_2 = request.POST.get('rent_B_2')
+        depreciation_B_1 = request.POST.get('depreciation_B_1')
+        depreciation_B_2 = request.POST.get('depreciation_B_2')
+        donation_B_1 = request.POST.get('donation_B_1')
+        donation_B_2 = request.POST.get('donation_B_2') 
+        sales_taxes_B_1 = request.POST.get('sales_taxes_B_1')
+        sales_taxes_B_2 = request.POST.get('sales_taxes_B_2')
+        purchases_B_1 = request.POST.get('purchases_B_1')
+        purchases_B_2 = request.POST.get('purchases_B_2')
+        other_purchases_B_1 = request.POST.get('other_purchases_B_1')
+        other_purchases_B_2 = request.POST.get('other_purchases_B_2')
+        other_operations_B_1 = request.POST.get('other_operations_B_1')
+        other_operations_B_2 = request.POST.get('other_operations_B_2')
+        total_expenses_B_1 = request.POST.get('total_expenses_B_1')
+        total_expenses_B_2 = request.POST.get('total_expenses_B_2')
+        net_profit_1 = request.POST.get('net_profit_1')
+        net_profit_2 = request.POST.get('net_profit_2')
+        income_C_1 = request.POST.get('income_C_1')
+        income_C_2 = request.POST.get('income_C_2')
+        profit_C_after_1 = request.POST.get('profit_C_after_1')
+        profit_C_after_2 = request.POST.get('profit_C_after_2')
+        sales_D_1 = request.POST.get('sales_D_1')
+        sales_D_2 = request.POST.get('sales_D_2')
+        name = request.POST.get('name')
+        rank = request.POST.get('rank')
+
+
+        csv_file_path = 'data/cuestionarios/ingreso_neto/IP-490.csv'
+        file_exists = os.path.isfile(csv_file_path) and os.path.getsize(csv_file_path) > 0
+
+        with open(csv_file_path, mode='a', newline='') as file:
+            writer = csv.writer(file)
+            
+            if not file_exists:
+                writer.writerow([
+                                "company_name", "address", "email", "liaison_officer",
+                                "ssn", "tel", "fax", "legal_form", "cfc", "business_type",
+                                "business_function", "closing_date", "start_year", "end_year", "income_operations_1",
+                                "income_operations_2", "people_A_1", "people_A_2", "industries_businesses_A_1", "industries_businesses_A_2",
+                                "interest_A_1", "interest_A_2", "rent_land_1", "rent_land_2", "capital_gain_1",
+                                "capital_gain_2", "other_income_A_1", "other_income_A_2", "total_income_A_1", "total_income_A_2",
+                                "salaries_wages_bonus_B_1", "salaries_wages_bonus_B_2", "interests_B_1", "interests_B_2", "rent_B_1",
+                                "rent_B_2", "depreciation_B_1", "depreciation_B_2", "donation_B_1", "donation_B_2",
+                                "sales_taxes_B_1", "sales_taxes_B_2", "purchases_B_1", "purchases_B_2", "other_purchases_B_1",
+                                "other_purchases_B_2", "other_operations_B_1", "other_operations_B_2", "total_expenses_B_1", "total_expenses_B_2",
+                                "net_profit_1", "net_profit_2", "income_C_1", "income_C_2", "profit_C_after_1",
+                                "profit_C_after_2", "sales_D_1", "sales_D_2", "name", "rank"
+                                ])
+                
+            writer.writerow([
+                            company_name, address, email, liaison_officer,
+                            ssn, tel, fax, legal_form, cfc, business_type,
+                            business_function, closing_date, start_year, end_year, income_operations_1,
+                            income_operations_2, people_A_1, people_A_2, industries_businesses_A_1, industries_businesses_A_2,
+                            interest_A_1, interest_A_2, rent_land_1, rent_land_2, capital_gain_1,
+                            capital_gain_2, other_income_A_1, other_income_A_2, total_income_A_1, total_income_A_2,
+                            salaries_wages_bonus_B_1, salaries_wages_bonus_B_2, interests_B_1, interests_B_2, rent_B_1,
+                            rent_B_2, depreciation_B_1, depreciation_B_2, donation_B_1, donation_B_2,
+                            sales_taxes_B_1, sales_taxes_B_2, purchases_B_1, purchases_B_2, other_purchases_B_1,
+                            other_purchases_B_2, other_operations_B_1, other_operations_B_2, total_expenses_B_1, total_expenses_B_2,
+                            net_profit_1, net_profit_2, income_C_1, income_C_2, profit_C_after_1,
+                            profit_C_after_2, sales_D_1, sales_D_2, name, rank
+                            ])
+            
+        return render(request, "forms/succesfull.html")
+    
     return render(request, "forms/yearly/ingreso_neto/IP-490.html")
