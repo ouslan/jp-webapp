27f96fed5c2a59b7baeb92c20796187683dcb33e
Kiam11 <kiam.perez@upr.edu>
2024-07-17 11:56:32 -0400

    Created IP 540a

--- web_app/urls.py
+++ web_app/urls.py
@@ -45,6 +45,7 @@ urlpatterns = [
     re_path(r"^IP-530-inmobiliaria-y-arrendamientos/$", views.IP_530, name="IP-530"),
     re_path(r"^IP-540-servicios-profesionales-tecnicos-y-cientificos/$", views.IP_540, name="IP-540"),
     re_path(r"^IP-540P-organizacion-de-viajes-y-servicios-de-reservacion/$", views.IP_540P, name="IP-540P"),
+    re_path(r"^IP-540a-publicidad-y-servicios-relacionados/$", views.IP_540a, name="IP-540a"),
     re_path(r"^Forms/$", views.Forms, name="Forms"),
 ]
 
