d8003c949c66214f7fa05af02d98f51df874fda6
Alejandro Ouslan <alejandro.ouslan@upr.edu>
2024-08-01 13:10:51 -0400

    finished data process

--- src/data/data_process.py
+++ src/data/data_process.py
@@ -1,23 +1,75 @@
-import pandas as pd
+import polars as pl
+import os
 
 class DataProcess:
 
-    def __init__(self, file_path: str):
-        self.file_path = file_path
-
+    def __init__(self, folder_path: str, saving_path: str, debug: bool = False):
+        self.folder_path = folder_path
+        self.saving_path = saving_path
+        self.debug = debug
+        self.df = self.process_data(self.folder_path)
+        self.df.write_csv(f"{self.saving_path}master.csv")
 
     def process_file(self, file_path: str):
-        df = pd.read_csv(file_path)
-        clean_df = pd.DataFrame(columns = ['Meses', file_path.split("/")[-1][:-4], "year"]) 
+        df = pl.read_csv(file_path)
+        column_name = file_path.split("/")[-1][:-4]
+        empty_df = [
+            pl.Series("month", [], dtype=pl.Int64),
+            pl.Series("year", [], dtype=pl.Int64),
+            pl.Series(column_name, [], dtype=pl.Float64)
+        ]
+        clean_df = pl.DataFrame(empty_df)
         for column in df.columns:
             if column == "Meses":
                 continue
-            tmp = df[["Meses", column]].copy()
-            tmp = df.rename(columns={column:file_path.split("/")[-1][:-4]})
-            tmp["year"] = column
-            clean_df = pd.concat([clean_df, tmp], axis=0)
-            clean_df.sort_values(by=["year", "Meses"])
+            column_name = file_path.split("/")[-1][:-4]
+            # Create a temporary DataFrame
+            tmp = df
+            tmp = tmp.rename({column:column_name})
+            tmp = tmp.with_columns(
+            pl.when(pl.col("Meses").str.strip_chars().str.to_lowercase() == "enero").then(1)
+              .when(pl.col("Meses").str.strip_chars().str.to_lowercase() == "febrero").then(2)
+              .when(pl.col("Meses").str.strip_chars().str.to_lowercase() == "marzo").then(3)
+              .when(pl.col("Meses").str.strip_chars().str.to_lowercase() == "abril").then(4)
+              .when(pl.col("Meses").str.strip_chars().str.to_lowercase() == "mayo").then(5)
+              .when(pl.col("Meses").str.strip_chars().str.to_lowercase() == "junio").then(6)
+              .when(pl.col("Meses").str.strip_chars().str.to_lowercase() == "julio").then(7)
+              .when(pl.col("Meses").str.strip_chars().str.to_lowercase() == "agosto").then(8)
+              .when(pl.col("Meses").str.strip_chars().str.to_lowercase() == "septiembre").then(9)
+              .when(pl.col("Meses").str.strip_chars().str.to_lowercase() == "octubre").then(10)
+              .when(pl.col("Meses").str.strip_chars().str.to_lowercase() == "noviembre").then(11)
+              .when(pl.col("Meses").str.strip_chars().str.to_lowercase() == "diciembre").then(12)
+              .alias("month")           
+                )
+            tmp = tmp.with_columns(
+                pl.col(column_name).str.replace_all("$", "", literal=True).str.replace_all(",", "").str.strip_chars().alias(column_name)
+            )
+            tmp = tmp.with_columns(
+                pl.when(pl.col(column_name)  == "n/d").then(None)
+                .when(pl.col(column_name)  == "**").then(None)
+                .when(pl.col(column_name)  == "-").then(None)
+                .when(pl.col(column_name)  == "no disponible").then(None)
+                .otherwise(pl.col(column_name)).alias(column_name)
+            )
+            tmp = tmp.select(
+                            pl.col("month").cast(pl.Int64).alias("month"),
+                            pl.lit(int(column)).cast(pl.Int64).alias("year"),
+                            pl.col(column_name).cast(pl.Float64).alias(column_name)
+            )
+            
+            # Append the temporary DataFrame to the list
+            clean_df = pl.concat([clean_df, tmp], how="vertical")
+        
+        
         return clean_df
 
-def process_data(self): 
-    pass
+    def process_data(self, folder_path: str): 
+        for dirt in os.listdir(folder_path):
+            for file in os.listdir(f"{folder_path}{dirt}"):
+                if file.endswith(".csv"):
+                    df = self.process_file(f"{folder_path}{dirt}/{file}")
+                try:
+                    master_df = master_df.join(df, on=["year", "month"], how="outer_coalesce")
+                except NameError:
+                    master_df = df
+        return master_df
