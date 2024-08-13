291d579e538cd4b0ffe7ab69a702e2d334250abe
Fernan2908 <fernando.sierra3@upr.edu>
2024-08-13 17:52:02 -0400

    Add the class to process the Historical Series

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from src.data.data_process import DataProcess
 from src.data.proccess_date import DataProcessByDate
+from src.data.data_process_macro import DataProcessHistoricalSeries
 
 def main() -> None:
     DataProcess("data/external/INDICADORES ECÓNOMICOS/", "data/processed/")
     
     DataProcessByDate("data/processed/master.csv", "data/processed/")
+    
+    DataProcessHistoricalSeries("data/raw/Series-historicas-1950-2011.csv", "data/processed/")
 
 if __name__ == "__main__":
     main()
--- src/data/data_process_macro.py
+++ src/data/data_process_macro.py
@@ -1,26 +1,27 @@
 import polars as pl
+
+
+class DataProcessHistoricalSeries:
+    
+    def __init__(self, folder_path: str, saving_path: str, debug: bool = False):
+        self.folder_path = folder_path
+        self.saving_path = saving_path
+        self.debug = debug
+        self.df = self.process_data(self.folder_path)
+        self.df.write_csv(f"{self.saving_path}Series-Historicas-1950-2011_processes.csv")   
+         
+    def process_data(self, folder_path: str):
+        df = pl.read_csv(folder_path)
         
-def process_file(file_path: str):
-    df = pl.read_csv(file_path)
-    
-    df.columns = [col.strip().replace(',', "").replace('$', '').replace(" )", ")").replace("  ", " ").replace("( ", " (").replace('"', '').replace(' ', '') for col in df.columns]
+        df.columns = [col.strip().replace(',', "").replace('$', '').replace(" )", ")").replace("  ", " ").replace("( ", " (").replace('"', '').upper() for col in df.columns]
 
-    df = df.with_columns([
-        pl.when(pl.col(col).str.contains(' ') | pl.col(col).str.contains('()') | pl.col(col).str.contains('n/d') | pl.col(col).str.contains('no disponible'))
-        .then(pl.col(col).str.replace(',', '').str.replace(' ', '').replace("(", "").replace(")", "").replace("n/d", None).replace("no disponible", None))
-        .otherwise(pl.col(col))
-        .alias(col)
-        for col in df.columns
-    ])
-    
-    df.write_csv("data/raw/Series-historicas_COPY_TEST.csv")
-
-
-
-def main() -> None:
-  process_file("data/raw/Series-historicas-1950-2011.csv")
-    
-    
-if __name__ == "__main__":
-  main()
+        df = df.with_columns([
+            pl.when(pl.col(col).str.contains(' ') | pl.col(col).str.contains('()') | pl.col(col).str.contains('n/d') | pl.col(col).str.contains('no disponible'))
+            .then(pl.col(col).str.replace(',', '').str.replace(' ', '').replace("(", "").replace(")", "").replace("n/d", None).replace("no disponible", None))
+            .otherwise(pl.col(col))
+            .alias(col)
+            for col in df.columns
+        ])
+        
+        return df
     
