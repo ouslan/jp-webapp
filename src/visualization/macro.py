89fe2c109a0b040d083a04d9f3f2af398a6d8ea9
Fernan2908 <fernando.sierra3@upr.edu>
2024-08-15 19:48:10 -0400

    Add the second graph to the html

--- src/visualization/macro.py
+++ src/visualization/macro.py
@@ -5,24 +5,25 @@ import plotly.graph_objects as go
 import plotly.express as px
 
 def web_app_macro(request):
-    df = pd.read_csv("data/processed/Series-Historicas-2001-2023_processes.csv").sort_values(by="PERIODO = AÑO FISCAL")
+  
+    # FIRST GRAPH DATA 
+    df1 = pd.read_csv("data/processed/Series-Historicas-2001-2023_processes.csv").sort_values(by="PERIODO = AÑO FISCAL")
+    y_axis_options_1 = df1.columns[1:]
+    x_axis_1 = df1["PERIODO = AÑO FISCAL"]
+    y_axis_1 = df1[y_axis_options_1[0]]
 
-    y_axis_options = df.columns[1:]
-
-    x_axis = df["PERIODO = AÑO FISCAL"]
-    y_axis = df[y_axis_options[0]]
-
-    fig = px.scatter(x=x_axis, y=y_axis)
-    fig.add_trace(go.Scatter(
-        x=x_axis,
-        y=y_axis,
+    # FIRST GRAPH SETTINGS
+    fig1 = px.scatter(x=x_axis_1, y=y_axis_1)
+    fig1.add_trace(go.Scatter(
+        x=x_axis_1,
+        y=y_axis_1,
         mode='lines+markers',
         line=dict(color='#FF2525', width=3),
-        marker=dict(size=4, color='#00CDFF'),
+        marker=dict(size=4, color='#00cdf1'),
         hoverinfo='text',
     ))
 
-    fig.update_layout(
+    fig1.update_layout(
         margin=dict(l=0, r=0, t=0, b=0),
         plot_bgcolor='#F7F7F7',
         hovermode='x',
@@ -59,7 +60,7 @@ def web_app_macro(request):
         width=1370
     )
 
-    fig.update_layout(
+    fig1.update_layout(
         annotations=[
             dict(
                 x=0.30,
@@ -81,10 +82,103 @@ def web_app_macro(request):
             dict(
                 buttons=[
                     dict(
-                        args=[{"y": [df[col]]}],
+                        args=[{"y": [df1[col]]}],
                         label=col,
                         method="update"
-                    ) for col in y_axis_options
+                    ) for col in y_axis_options_1
+                ],
+                direction="down",
+                pad={"r": 10, "t": 10},
+                showactive=True,
+                x=0.35,
+                xanchor="left",
+                y=1.15,
+                yanchor="top"
+            )
+        ]
+    )
+    # --------------------------------------------------------------------------------------------------------------------------------
+    
+    # SECOND GRAPH DATA
+    df2 = pd.read_csv("data/processed/Series-Historicas-1950-2011_processes.csv").sort_values(by="PERIODO = AÑO FISCAL")
+    y_axis_options_2 = df2.columns[1:]
+    x_axis_2 = df2["PERIODO = AÑO FISCAL"]
+    y_axis_2 = df2[y_axis_options_2[0]]
+    
+    #  SECOND GRAPH SETTINGS
+    fig2 = px.scatter(x=x_axis_2, y=y_axis_2)
+    fig2.add_trace(go.Scatter(
+        x=x_axis_2,
+        y=y_axis_2,
+        mode='lines+markers',
+        line=dict(color='#FF2525', width=3),
+        marker=dict(size=4, color='#00cdf1'),
+        hoverinfo='text',
+    ))
+
+    fig2.update_layout(
+        margin=dict(l=0, r=0, t=0, b=0),
+        plot_bgcolor='#F7F7F7',
+        hovermode='x',
+        showlegend=False,
+        xaxis=dict(
+            title="",
+            color='black',
+            showgrid=True,
+            showticklabels=True,
+            linecolor='black',
+            linewidth=2,
+            ticks='outside',
+            tickfont=dict(
+                family='Arial',
+                size=12,
+                color='black'
+            ),
+        ),
+        yaxis=dict(
+            title="",
+            color='black',
+            showline=True,
+            showgrid=True,
+            showticklabels=True,
+            linecolor='black',
+            linewidth=2,
+            ticks='outside',
+            tickfont=dict(
+                family='Arial',
+                size=12,
+                color='black'
+            ),
+        ),
+        width=1370
+    )
+
+    fig2.update_layout(
+        annotations=[
+            dict(
+                x=0.30,
+                y=1.11,
+                xref='paper',
+                yref='paper',
+                text='Y:',
+                showarrow=False,
+                font=dict(
+                    family='Arial',
+                    size=18,
+                    color='black'
+                ),
+                xanchor='left',
+                yanchor='top'
+            )
+        ],
+        updatemenus=[
+            dict(
+                buttons=[
+                    dict(
+                        args=[{"y": [df2[col]]}],
+                        label=col,
+                        method="update"
+                    ) for col in y_axis_options_2
                 ],
                 direction="down",
                 pad={"r": 10, "t": 10},
@@ -97,9 +191,11 @@ def web_app_macro(request):
         ]
     )
 
-    macro_html = fig.to_html()
+    macro_html_1 = fig1.to_html()
+    macro_html_2 = fig2.to_html()
 
     context = {
-        "macro": macro_html,
+        "macro1": macro_html_1,
+        "macro2": macro_html_2,
     }
     return render(request, "macro.html", context)
