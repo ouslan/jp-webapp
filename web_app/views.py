da79715af508c6070af3ba75167355b5435cd966
EdzelOrtiz <edzel.ortiz@upr.edu>
2024-08-19 08:56:57 -0400

    Update JP-362-qrt to JP-361-qrt, falta ajustarlo

--- web_app/views.py
+++ web_app/views.py
@@ -370,5 +370,5 @@ def JP_544_1(request):
     return render(request, "forms/yearly/balanza_de_pagos/JP-544-1.html")
 
 
-def JP_362_qrt(request):
-    return render(request, "forms/quaterly/balanza_de_pagos/JP-362-qrt.html")
+def JP_361_qrt(request):
+    return render(request, "forms/quaterly/balanza_de_pagos/JP-361-qrt.html")
