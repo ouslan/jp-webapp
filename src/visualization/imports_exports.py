baf31e0484ca4466f200c314ad744d13b92ea8f3
Fernan2908 <fernando.sierra3@upr.edu>
2024-09-04 11:26:27 -0400

    Change the graph

--- src/visualization/imports_exports.py
+++ src/visualization/imports_exports.py
@@ -4,19 +4,63 @@ from django.shortcuts import render
 
 def web_app_imports_exports(request):
     df = pl.read_parquet("data/external/temporero.parquet")
-    data = dict(
-        names=df["country"].to_list() + df["year"].unique().cast(str).to_list(),
-        parents=df["year"].cast(str).to_list() + [""] * df["year"].n_unique(),
-        values=df["data"].to_list() + [df.filter(pl.col("year") == y).select(pl.col("data")).sum().item() for y in df["year"].unique()]
+    years = df['year'].unique().to_list()
+
+    fig = px.pie(df, values='data', names='country')
+
+    fig.update_traces(textposition='inside', textinfo='percent+label')
+
+    buttons = [
+        dict(
+            args=[{"x": [df.filter(pl.col("year") == year)["data"].to_list()], "labels": [df.filter(pl.col("year") == year)["country"].to_list()]}],
+            label=str(year),
+            method="update"
+        )
+        for year in years
+    ]
+
+    buttons.insert(0, dict(
+        args=[{"x": [df["data"].to_list()], "labels": [df["country"].to_list()]}],
+        label="Annual",
+        method="update"
+    ))
+
+    fig.update_layout(
+        annotations=[
+            dict(
+                x=0.75,
+                y=1.11,
+                xref='paper',
+                yref='paper',
+                text="",
+                showarrow=False,
+                font=dict(
+                    family='Arial',
+                    size=18,
+                    color='black'
+                ),
+                xanchor='left',
+                yanchor='top'
+            ),
+        ],
+        updatemenus=[
+            dict(
+                buttons=buttons,
+                direction="down",
+                pad={"r": 10, "t": 10},
+                showactive=True,
+                x=0.05,
+                xanchor="left",
+                y=1.14,
+                yanchor="top"
+            ),
+        ]
     )
-    fig = px.sunburst(
-        data,
-        names='names',
-        parents='parents',
-        values='values',
-    )
+
     imports_and_exports = fig.to_html(full_html=False, default_height=500, default_width=700)
+
     context = {
         "imports_and_exports": imports_and_exports
     }
+
     return render(request, "imports_exports.html", context)
