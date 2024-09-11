9d1c775e74c3ce43b233401f4c25ccf2a9e4451c
Fernan2908 <fernando.sierra3@upr.edu>
2024-09-11 11:57:16 -0400

    Added the functionality of two dropdowns to the graph

--- src/visualization/imports_exports.py
+++ src/visualization/imports_exports.py
@@ -8,63 +8,34 @@ def web_app_imports_exports(request):
     df1 = DataProcess(saving_dir="data/", instance="jp_instetute", debug=True).process_int_jp(time="yearly", types="country")
     df2 = DataProcess(saving_dir="data/", instance="jp_instetute", debug=True).process_int_jp(time="monthly", types="country")
     df3 = DataProcess(saving_dir="data/", instance="jp_instetute", debug=True).process_int_jp(time="qrt", types="country")
-    
-    df1 = df1.with_columns(data=pl.col("imports"))
-    years = df1['year'].unique().to_list()
-    
-    df2 = df2.with_columns(data=pl.col("imports"))
-    months = df2['month'].unique().to_list()
-    
-    df3 = df3.with_columns(data=pl.col("imports"))
-    qrts = df3['qrt'].unique().to_list()
 
-    fig = px.pie(df1, values='data', names='country')
-
+    fig = px.pie(df1, values='imports', names='country')
     fig.update_traces(textposition='inside', textinfo='percent+label')
     
-    frequency = ["yearly", "monthly", "qtr"]
+    if request.method == "POST":
+        frequency = request.POST.get("frequency")
+        second_dropdown = request.POST.get("second_dropdown")
+        
+        if frequency == "Yearly":
+            df1 = df1.with_columns(imports=pl.col("imports"))
+            df1 = df1.filter(pl.col("year") == int(second_dropdown))
+            fig = px.pie(df1, values='imports', names='country')
+        elif frequency == "Monthly":
+            df2 = df2.with_columns(imports=pl.col("imports"))
+            df2 = df2.filter(pl.col("month") == int(second_dropdown))
+            fig = px.pie(df2, values='imports', names='country')
+        elif frequency == "Quaterly":
+            df3 = df3.with_columns(imports=pl.col("imports"))
+            df3 = df3.filter(pl.col("qrt") == int(second_dropdown))
+            fig = px.pie(df3, values='imports', names='country')
+        
+        fig.update_layout(
+            title=f"Gráfica de Importaciones: {frequency} / {second_dropdown}",
+        )
     
-    buttons = [
-        dict(
-            args=[{"data": [df1.to_dicts(), df2.to_dicts(), df3.to_dicts()][frequency.index(freq)], "frequency": freq}],
-            label={"yearly": "Yearly", "monthly": "Monthly", "qtr": "Qtr"}[freq],
-            method="update"
-        ) for freq in frequency
-    ]
-    
-    
-    fig.update_layout(
-        annotations=[
-            dict(
-                x=0.75,
-                y=1.11,
-                xref='paper',
-                yref='paper',
-                text="",
-                showarrow=False,
-                font=dict(
-                    family='Arial',
-                    size=18,
-                    color='black'
-                ),
-                xanchor='left',
-                yanchor='top'
-            ),
-        ],
-        updatemenus=[
-            dict(
-                buttons=buttons,
-                direction="down",
-                pad={"r": 10, "t": 10},
-                showactive=True,
-                x=0.05,
-                xanchor="left",
-                y=1.14,
-                yanchor="top"
-            ),
-        ]
-    )
+    fig.update_traces(textposition='inside', textinfo='percent+label')
 
+    
     imports_and_exports = fig.to_html(full_html=False, default_height=500, default_width=700)
 
     context = {
@@ -72,3 +43,6 @@ def web_app_imports_exports(request):
     }
 
     return render(request, "imports_exports.html", context)
+
+
+
