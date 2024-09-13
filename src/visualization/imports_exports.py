327f1dfa947c98809f231479946bd8047723a7a9
Fernan2908 <fernando.sierra3@upr.edu>
2024-09-13 10:37:38 -0400

    Add the base graph to the exports

--- src/visualization/imports_exports.py
+++ src/visualization/imports_exports.py
@@ -4,10 +4,12 @@ from ..jp_imports.src.jp_imports.data_process import DataProcess
 from django.shortcuts import render
 
 def web_app_imports_exports(request):
-    df1 = DataProcess(saving_dir="data/", instance="jp_instetute", debug=True).process_int_jp(time="yearly", types="country")
-    df2 = DataProcess(saving_dir="data/", instance="jp_instetute", debug=True).process_int_jp(time="monthly", types="country")
-    df3 = DataProcess(saving_dir="data/", instance="jp_instetute", debug=True).process_int_jp(time="qrt", types="country")
+    df1 = DataProcess(saving_dir="data/", instance="jp_instetute", debug=True).process_int_jp(time="yearly", types="country").collect() # type: ignore
+    df2 = DataProcess(saving_dir="data/", instance="jp_instetute", debug=True).process_int_jp(time="monthly", types="country").collect() # type: ignore
+    df3 = DataProcess(saving_dir="data/", instance="jp_instetute", debug=True).process_int_jp(time="qrt", types="country").collect() # type: ignore
 
+
+    # IMPORTS GRAPH 
     fig = px.pie(df1, values='imports', names='country')
     fig.update_traces(textposition='inside', textinfo='percent+label')
     
@@ -29,7 +31,11 @@ def web_app_imports_exports(request):
             fig = px.pie(df3, values='imports', names='country')
         
         fig.update_layout(
-            title=f"Gráfica de Importaciones: {frequency} / {second_dropdown}",
+            title={
+                'text': f"Gráfica de Importaciones: {frequency} / {second_dropdown}",
+                'x': 0.5,
+                'font': {'color': 'black'}
+            },
         )
     
     fig.update_traces(textposition='inside', textinfo='percent+label')
@@ -37,8 +43,45 @@ def web_app_imports_exports(request):
     
     imports_and_exports = fig.to_html(full_html=False, default_height=500, default_width=700)
 
+    # ------------------------------------------------------------------------------------------------------------------------------------------------------------
+    
+    # EXPORTS GRAPH
+    fig = px.pie(df1, values='imports', names='country')
+    fig.update_traces(textposition='inside', textinfo='percent+label')
+    
+    if request.method == "POST":
+        frequency = request.POST.get("frequency")
+        second_dropdown = request.POST.get("second_dropdown")
+        
+        if frequency == "Yearly":
+            df1 = df1.with_columns(imports=pl.col("imports")) # type: ignore
+            df1 = df1.filter(pl.col("year") == int(second_dropdown))
+            fig = px.pie(df1, values='imports', names='country')
+        elif frequency == "Monthly":
+            df2 = df2.with_columns(imports=pl.col("imports")) # type: ignore
+            df2 = df2.filter(pl.col("month") == int(second_dropdown))
+            fig = px.pie(df2, values='imports', names='country')
+        elif frequency == "Quarterly":
+            df3 = df3.with_columns(imports=pl.col("imports")) # type: ignore
+            df3 = df3.filter(pl.col("qrt") == int(second_dropdown))
+            fig = px.pie(df3, values='imports', names='country')
+        
+        fig.update_layout(
+            title={
+                'text': f"Gráfica de Importaciones: {frequency} / {second_dropdown}",
+                'x': 0.5,
+                'font': {'color': 'black'}
+            },
+        )
+    
+    fig.update_traces(textposition='inside', textinfo='percent+label')
+
+    
+    imports_and_exports_2 = fig.to_html(full_html=False, default_height=500, default_width=700)
+
     context = {
-        "imports_and_exports": imports_and_exports
+        "imports_and_exports": imports_and_exports,
+        "imports_and_exports_2": imports_and_exports_2
     }
 
     return render(request, "imports_exports.html", context)
