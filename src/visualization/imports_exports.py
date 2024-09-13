c94ebd339dbf6b0ce944a267e887b48fdc6e2be9
Fernan2908 <fernando.sierra3@upr.edu>
2024-09-13 08:52:48 -0400

    Fix bug with the "Quaterly" frequency

--- src/visualization/imports_exports.py
+++ src/visualization/imports_exports.py
@@ -1,4 +1,3 @@
-from calendar import month
 import polars as pl
 import plotly.express as px
 from ..jp_imports.src.jp_imports.data_process import DataProcess
@@ -17,15 +16,15 @@ def web_app_imports_exports(request):
         second_dropdown = request.POST.get("second_dropdown")
         
         if frequency == "Yearly":
-            df1 = df1.with_columns(imports=pl.col("imports"))
+            df1 = df1.with_columns(imports=pl.col("imports")) # type: ignore
             df1 = df1.filter(pl.col("year") == int(second_dropdown))
             fig = px.pie(df1, values='imports', names='country')
         elif frequency == "Monthly":
-            df2 = df2.with_columns(imports=pl.col("imports"))
+            df2 = df2.with_columns(imports=pl.col("imports")) # type: ignore
             df2 = df2.filter(pl.col("month") == int(second_dropdown))
             fig = px.pie(df2, values='imports', names='country')
-        elif frequency == "Quaterly":
-            df3 = df3.with_columns(imports=pl.col("imports"))
+        elif frequency == "Quarterly":
+            df3 = df3.with_columns(imports=pl.col("imports")) # type: ignore
             df3 = df3.filter(pl.col("qrt") == int(second_dropdown))
             fig = px.pie(df3, values='imports', names='country')
         
