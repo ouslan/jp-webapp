da08a3fe9e1951ec890699ba074ba0c6cfb1fa71
Adriana Isabel <adriana.carrasquillo1@upr.edu>
2024-09-13 11:25:26 -0400

    Moved the table to the file Proyecciones

--- web_app/demographic_graph.py
+++ web_app/demographic_graph.py
@@ -1,45 +1,73 @@
 import polars as pl
 import plotly.graph_objects as go
 
-
 # Read the CSV file into a DataFrame
 df = pl.read_csv("data/external/Anual_Historico.csv")
 
-# Create figure Added now
+# Create figure
 fig = go.Figure()
 
 # Add traces using data from the CSV file
+# Add Annotations and Buttons
+annotations = [
+    dict(x="2022-01-01", y=100, xref="x", yref="y",
+         text="Annotation Text", showarrow=True, arrowhead=1, ax=0, ay=-40)
+]
+
+# Define the update menu options
+update_menu = [
+    dict(label="Option 1",
+         method="update",
+         args=[{"visible": [True, False, True, False, True, False, True, False]},
+               {"title": "Option 1", "annotations": annotations}]),
+    dict(label="Option 2",
+         method="update",
+         args=[{"visible": [False, True, False, True, False, True, False, True]},
+               {"title": "Option 2", "annotations": annotations}]),
+    # Add more options as needed
+]
+
+# Update layout with the updatemenus dropdown
+fig.update_layout(
+    updatemenus=[
+        dict(
+            active=0,
+            buttons=update_menu
+        )
+    ]
+)
+
 # Top left
 fig.add_trace(
     go.Scatter(x=df['x_column'], y=df['y_column1'], name="yaxis data"),
-    )
+)
 fig.add_trace(
     go.Scatter(x=df['x_column'], y=df['y_column2'], name="yaxis2 data"),
-    )
+)
 
 # Top right
 fig.add_trace(
     go.Scatter(x=df['x_column'], y=df['y_column3'], name="yaxis3 data"),
-    )
+)
 fig.add_trace(
     go.Scatter(x=df['x_column'], y=df['y_column4'], name="yaxis4 data"),
-    )
+)
 
 # Bottom left
 fig.add_trace(
     go.Scatter(x=df['x_column'], y=df['y_column5'], name="yaxis5 data"),
-    )
+)
 fig.add_trace(
     go.Scatter(x=df['x_column'], y=df['y_column6'], name="yaxis6 data"),
-    )
+)
 
 # Bottom right
 fig.add_trace(
     go.Scatter(x=df['x_column'], y=df['y_column7'], name="yaxis7 data"),
-    )
+)
 fig.add_trace(
     go.Scatter(x=df['x_column'], y=df['y_column8'], name="yaxis8 data"),
-    )
+)
 
 # Update layout with range slider and selectors
 fig.update_layout(
@@ -62,6 +90,8 @@ fig.update_layout(
     width=800,
     height=800
 )
+
+# Add the Update Dropdown functionality here
 
 # Convert the figure to HTML
 graph_html = fig.to_html(full_html=False)
