7d1f14c4365bc78f28fb43c1c1d179da81941d7c
Fernan2908 <fernando.sierra3@upr.edu>
2024-09-27 10:34:28 -0400

    Add the sign in authentication

--- src/visualization/sign_in.py
+++ src/visualization/sign_in.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate
+from django.views.decorators.csrf import csrf_protect
 
+@csrf_protect
 def sign_in_page(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            return render(request, "forms/forms.html")
+        else:
+            return render(request, "signin.html")
+    
     return render(request, "signin.html")
