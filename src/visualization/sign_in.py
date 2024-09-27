91414ea8fee413881d68f3de95f3e5e2c25b8771
Fernan2908 <fernando.sierra3@upr.edu>
2024-09-27 10:57:39 -0400

    Add messages error to the sign in

--- src/visualization/sign_in.py
+++ src/visualization/sign_in.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth import authenticate
+from django.contrib import messages
 from django.views.decorators.csrf import csrf_protect
 
 @csrf_protect
@@ -12,6 +13,10 @@ def sign_in_page(request):
         if user is not None:
             return render(request, "forms/forms.html")
         else:
+            if not username or not password:
+                messages.error(request, "Por favor ingrese un nombre de usuario y contraseña.")
+            else:
+                messages.error(request, "El usuario no existe o la contraseña es incorrecta.")
             return render(request, "signin.html")
     
     return render(request, "signin.html")
