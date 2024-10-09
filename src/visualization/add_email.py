e88ffa4f88a7aaa2f2259dc90c18fd81de3ff1a8
Fernan2908 <fernando.sierra3@upr.edu>
2024-10-09 08:36:10 -0400

    Add the feature to change the email address of the user.

--- src/visualization/add_email.py
+++ src/visualization/add_email.py
@@ -2,5 +2,14 @@ from django.contrib import messages
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 
-def change_email(request):    
-  return render(request, "registration/add_email.html")
+def change_email(request):
+    if request.method == "POST":
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        user = authenticate(username=request.user.username, password=password)
+        if user is not None:
+            user.email = email
+            user.save()
+            return redirect("web_app:log_in_page")
+
+    return render(request, "registration/add_email.html")
