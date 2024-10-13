a4c54f93c7521de9ca45f7e4e7cf3e73bb54449c
Alejandro Ouslan <alejandro.ouslan@upr.edu>
2024-10-13 14:34:10 -0400

    update debug
    Signed-off-by: Alejandro Ouslan <alejandro.ouslan@upr.edu>

--- config_ui/settings.py
+++ config_ui/settings.py
@@ -38,7 +38,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "dev.econlabs.net", "127.0.0.1"]
 
