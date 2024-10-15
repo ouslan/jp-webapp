694eae1fed886d95d49e0208f59255d09b6dcb77
Fernando Sierra <98364824+Fernando2908@users.noreply.github.com>
2024-10-15 10:48:03 -0400

    65 change email feature (#86)
    * Add change_email view for adding email functionality
    
    * Fix the path position of forgot passworn and change email in log in page
    
    * Update button and overlay colors in landing page CSS
    
    * Update login page overlay text
    
    * Add the feature to change the email address of the user.
    
    * Update the profile and user-info
    
    * Update link path for "CUESTIONARIOS" in proyectos.html
    
    * Add a setting button in the profile
    
    * Add account settings page for authenticated users
    
    * Update link path for "CUESTIONARIOS" in proyectos.html
    
    * Fix the account settings page
    
    * Update link paths for "CUESTIONARIOS" in proyectos.html
    
    * Remove login_required decorator from Forms view
    
    ---------
    
    Co-authored-by: Alejandro Ouslan <alejandro.ouslan@upr.edu>

--- web_app/views.py
+++ web_app/views.py
@@ -188,7 +188,7 @@ def indicadores(request):
 def succesfull_page(request):
     return render(request, "forms/succesfull.html")
 
-@login_required(login_url='web_app:log_in_page')
+# @login_required(login_url='web_app:log_in_page')
 def Forms(request):
     user = request.user
     return render(request, "forms/forms.html", {"user": user})
