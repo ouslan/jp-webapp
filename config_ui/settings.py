f0a98a498b75a17f90c6aa1cebdec6e2be200328
Alejandro Ouslan <alejandro.ouslan@upr.edu>
2024-10-19 19:56:34 -0400

    added support jp-api

--- config_ui/settings.py
+++ config_ui/settings.py
@@ -11,22 +11,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from env import get_db_credentials
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
-# Database credentials
-NAME = os.environ.get("POSTGRES_DB")
-USER = os.environ.get("POSTGRES_USER")
-PASSWORD = os.environ.get("POSTGRES_PASSWORD")
-
-if not all([NAME, USER, PASSWORD]):
-    raise ValueError("Database credentials not set")
-if os.environ.get("DEV") == "True":
-    HOST = "localhost"
-else:
-    HOST = "timescaledb"
-
+PORT, USER, PASSWORD, HOST, DATABASE, DATABASE_URL, SECRET_KEY= get_db_credentials()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -35,9 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = ["localhost", "dev.econlabs.net", "127.0.0.1"]
---
+++ env.py
@@ -0,0 +1,25 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+# Get the Database credentials
+def get_db_credentials():
+    PORT = os.getenv("POSTGRES_PORT")
+    USER = os.getenv("POSTGRES_USER")
+    PASSWORD = os.getenv("POSTGRES_PASSWORD")
+    HOST = os.getenv("POSTGRES_HOST")
+    DATABASE = os.getenv("POSTGRES_DB")
+    DATABASE_URL = os.getenv("DATABASE_URL")
+    SECRET_KEY = os.getenv("SECRET_KEY")
+
+    if not all([HOST, USER, PASSWORD, DATABASE, PORT, DATABASE_URL]):
+        raise ValueError("Database credentials not set")
+    if os.environ.get("DEV") == "True":
+        HOST = "localhost"
+        DATABASE_URL = f"postgresql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}"
+    else:
+        HOST = "timescaledb"
+        DATABASE_URL = f"postgresql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}"
+    return PORT, USER, PASSWORD, HOST, DATABASE, DATABASE_URL
+
