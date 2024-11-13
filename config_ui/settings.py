2eb9d203f90b32f15b61d7e8748bde3ba0245387
Alejandro Ouslan <alejandro.ouslan@upr.edu>
2024-11-13 15:48:53 +0000

    71 sqlserver (#97)
    * added support jp-api
    
    * change settings
    
    * fix docker build

--- config_ui/settings.py
+++ config_ui/settings.py
@@ -11,22 +11,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from env import get_db_credentials
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
-# Database credentials
-NAME = os.environ.get("POSTGRES_DB")
-USER = os.environ.get("POSTGRES_USER")
-PASSWORD = os.environ.get("POSTGRES_PASSWORD")
-
-if not all([NAME, USER, PASSWORD]):
-    raise ValueError("Database credentials not set")
-if os.environ.get("DEV") == "True":
-    HOST = "localhost"
-else:
-    HOST = "timescaledb"
-
+USER, PASSWORD, HOST, DATABASE, DATABASE_URL, SECRET_KEY = get_db_credentials()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -35,62 +23,59 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = ["localhost", "dev.econlabs.net", "127.0.0.1"]
 
-CSRF_TRUSTED_ORIGINS = ['https://dev.econlabs.net']
+CSRF_TRUSTED_ORIGINS = ["https://dev.econlabs.net"]
 
 
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'web_app',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "web_app",
 ]
 
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
 ]
 
-ROOT_URLCONF = 'config_ui.urls'
+ROOT_URLCONF = "config_ui.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
         "DIRS": [
             BASE_DIR / "templates",
-            ],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        ],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'config_ui.wsgi.application'
+WSGI_APPLICATION = "config_ui.wsgi.application"
 
 
 # Database
@@ -99,7 +84,7 @@ WSGI_APPLICATION = 'config_ui.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": NAME,
+        "NAME": DATABASE,
         "USER": USER,
         "PASSWORD": PASSWORD,
         "HOST": HOST,
@@ -113,16 +98,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -130,9 +115,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -142,16 +127,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/staticfiles/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [] 
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+STATIC_URL = "/staticfiles/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = []
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-LOGIN_REDIRECT_URL = 'web_app:forms'
-LOGIN_URL = 'web_app:log_in_page'
+LOGIN_REDIRECT_URL = "web_app:forms"
+LOGIN_URL = "web_app:log_in_page"
---
+++ env.py
@@ -0,0 +1,28 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+# Get the Database credentials
+def get_db_credentials():
+    USER = str(os.environ.get("POSTGRES_USER")).strip()
+    HOST = str(os.getenv("POSTGRES_HOST")).strip()
+    DATABASE = str(os.environ.get("POSTGRES_DB")).strip()
+    SECRET_KEY = str(os.getenv("SECRET_KEY")).strip()
+
+    if not all([HOST, USER, DATABASE, SECRET_KEY]):
+        raise ValueError("Database credentials not set")
+    if os.environ.get("DEV") == "True":
+        HOST = "localhost"
+        PASSWORD = str(read_secret_file('/run/secrets/db-password')).strip()
+        DATABASE_URL = f"postgresql://{USER}:{PASSWORD}@{HOST}:5432/{DATABASE}"
+    else:
+        HOST = "database"
+        PASSWORD = str(read_secret_file('/run/secrets/db-password')).strip()
+        DATABASE_URL = f"postgresql://{USER}:{PASSWORD}@{HOST}:5432/{DATABASE}"
+    return USER, PASSWORD, HOST, DATABASE, DATABASE_URL, SECRET_KEY
+
+def read_secret_file(secret_path):
+    with open(secret_path, 'r') as file:
+        return file.read().strip()
