4f3e8f7cc5e11c0d0fc3b031b986179670084fe0
BryanDVG <70717360+BryanDVG@users.noreply.github.com>
2024-12-06 14:10:59 -0400

    #85 Added the bottom and top graph format

--- src/visualization/productos_ranking.py
+++ src/visualization/productos_ranking.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 
 def productos_ranking(request):
     # URLs for each frequency
-    response = requests.get("https://api.econlabs.net/data/trade/moving?agr=true").json()
+    response = requests.get("https://api.econlabs.net/data/trade/moving?agr=false").json()
 
     # Convert session data into DataFrames
     df = pd.DataFrame(response)
@@ -15,10 +15,14 @@ def productos_ranking(request):
     last_month = df['date'].max()
 
     # Filter the data for the last month
-    df_last_month = df[df['date'] == last_month]
+    df_last_month_imports = df[(df['date'] == last_month) & (df["moving_import_rank"] != 0)]
+    df_last_month_exports = df[(df['date'] == last_month) & (df["moving_export_rank"] != 0)]
+
+
+    
     # Sort the DataFrame by export and import rank, then select the top 5 items
-    df_imports = df_last_month.sort_values(by="moving_import_rank")
-    df_exports = df_last_month.sort_values(by="moving_export_rank")
+    df_imports = df_last_month_imports.sort_values(by="moving_import_rank")
+    df_exports = df_last_month_exports.sort_values(by="moving_export_rank")
     
     top5_imports = df_imports.head(5)
     top5_exports = df_exports.head(5)
@@ -32,47 +36,83 @@ def productos_ranking(request):
     # For imports (negative values for bottom 5 to move them to the left)
     # last5_imports['moving_price_imports'] *= -1
     # print(last5_imports["moving_price_imports"])
-    imports_data = pd.concat([last5_imports, top5_imports])
+    # imports_data = pd.concat([last5_imports, top5_imports])
 
     # For exports (negative values for bottom 5 to move them to the left)
     # last5_exports['moving_price_exports'] *= -1
-    # print(top5_exports["moving_price_exports"])
-    exports_data = pd.concat([last5_exports, top5_exports])
-    # Group by the new column and sum the qty_imports
-    fig_export = go.Figure()
-    fig_export.add_trace(go.Bar(
-        x=exports_data['moving_price_exports'],
-        y=exports_data['hs4'],
+    print(top5_exports["moving_price_exports"])
+    # exports_data = pd.concat([last5_exports, top5_exports])
+
+    # Create histogram for top 5 export ranks
+    fig_export_top = go.Figure()
+    fig_export_top.add_trace(go.Bar(
+        x=top5_exports['moving_price_exports'],
+        y=top5_exports['hs4'],
         orientation="h",
         textposition='auto',
         name="Top Exports"
     ))
 
-    fig_export.update_layout(
+    fig_export_top.update_layout(
         title="Top 5 Items by Export Rank",
-        xaxis_title="HTS Code",
-        yaxis_title="Export Rank",
+        xaxis_title="Export Price",
+        yaxis_title="HTS Code",
+        bargap=0.2
+    )
+
+    # Create histogram for bottom 5 export ranks
+    fig_export_bottom = go.Figure()
+    fig_export_bottom.add_trace(go.Bar(
+        x=last5_exports['moving_price_exports'],
+        y=last5_exports['hs4'],
+        orientation="h",
+        textposition='auto',
+        name="Bottom Exports"
+    ))
+
+    fig_export_bottom.update_layout(
+        title="Bottom 5 Items by Export Rank",
+        xaxis_title="Export Price",
+        yaxis_title="HTS Code",
         bargap=0.2
     )
 
     # Create histogram for top 5 import ranks
-    fig_import = go.Figure()
-    fig_import.add_trace(go.Bar(
-        x=imports_data['moving_price_imports'],
-        y=imports_data['hs4'],
+    fig_import_top = go.Figure()
+    fig_import_top.add_trace(go.Bar(
+        x=top5_imports['moving_price_imports'],
+        y=top5_imports['hs4'],
         orientation="h",
         textposition='auto',
         name="Top Imports"
     ))
-    fig_import.update_layout(
+    fig_import_top.update_layout(
         title="Top 5 Items by Import Rank",
-        xaxis_title="HTS Code",
-        yaxis_title="Import Rank",
+        xaxis_title="Import Price",
+        yaxis_title="HTS Code",
+        bargap=0.2
+    )
+
+    # Create histogram for bottom 5 import ranks
+    fig_import_bottom = go.Figure()
+    fig_import_bottom.add_trace(go.Bar(
+        x=last5_imports['moving_price_imports'],
+        y=last5_imports['hs4'],
+        orientation="h",
+        textposition='auto',
+        name="Bottom Imports"
+    ))
+    fig_import_bottom.update_layout(
+        title="Bottom 5 Items by Import Rank",
+        xaxis_title="Import Price",
+        yaxis_title="HTS Code",
         bargap=0.2
     )
 
     # Validate required columns
     return render(request, 'productos_ranking.html', {
-        'graph_export': fig_export.to_html(),
-        'graph_import': fig_import.to_html()
+        'graph_export_top': fig_export_top.to_html(),
+        'graph_export_bottom' : fig_export_bottom.to_html(),
+        'graph_import_top': fig_import_top.to_html(),
+        'graph_import_bottom' : fig_import_bottom.to_html()
     })
