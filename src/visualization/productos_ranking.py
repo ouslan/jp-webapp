b1d914cc4d842b3b9622913aacb8ce750504b2bf
BryanDVG <70717360+BryanDVG@users.noreply.github.com>
2024-12-06 13:16:45 -0400

    #85 Updated ranking graphs

--- src/visualization/productos_ranking.py
+++ src/visualization/productos_ranking.py
@@ -7,29 +7,42 @@ def productos_ranking(request):
     # URLs for each frequency
     response = requests.get("https://api.econlabs.net/data/trade/moving?agr=true").json()
 
-
     # Convert session data into DataFrames
     df = pd.DataFrame(response)
+    # print(df.columns)
 
     # Find the last month in the DataFrame
     last_month = df['date'].max()
 
-# Filter the data for the last month
+    # Filter the data for the last month
     df_last_month = df[df['date'] == last_month]
     # Sort the DataFrame by export and import rank, then select the top 5 items
-    top_exports = df_last_month.nlargest(5, 'moving_export_rank') 
-    top_imports = df_last_month.nlargest(5, 'moving_import_rank')
-    bottom_exports = df_last_month.nsmallest(5, 'moving_export_rank')
-    bottom_imports = df_last_month.nsmallest(5, 'moving_import_rank')
+    df_imports = df_last_month.sort_values(by="moving_import_rank")
+    df_exports = df_last_month.sort_values(by="moving_export_rank")
+    
+    top5_imports = df_imports.head(5)
+    top5_exports = df_exports.head(5)
+    last5_imports = df_imports.tail(5)
+    last5_exports = df_exports.tail(5)
+    # print(top5_exports)
+    # print(last5_exports)
+    # print(top5_imports)
+    # print(last5_imports)
+    # Prepare data for diverging bar chart
+    # For imports (negative values for bottom 5 to move them to the left)
+    # last5_imports['moving_price_imports'] *= -1
+    # print(last5_imports["moving_price_imports"])
+    imports_data = pd.concat([last5_imports, top5_imports])
 
-    combined_exports = pd.concat([top_exports,bottom_exports])
-    combined_imports = pd.concat([top_imports, bottom_imports])
-
+    # For exports (negative values for bottom 5 to move them to the left)
+    # last5_exports['moving_price_exports'] *= -1
+    # print(top5_exports["moving_price_exports"])
+    exports_data = pd.concat([last5_exports, top5_exports])
     # Group by the new column and sum the qty_imports
     fig_export = go.Figure()
     fig_export.add_trace(go.Bar(
-        x=combined_exports['moving_export_rank'],
-        y=combined_exports['hs4'],
+        x=exports_data['moving_price_exports'],
+        y=exports_data['hs4'],
         orientation="h",
         textposition='auto',
         name="Top Exports"
@@ -45,8 +58,8 @@ def productos_ranking(request):
     # Create histogram for top 5 import ranks
     fig_import = go.Figure()
     fig_import.add_trace(go.Bar(
-        x=combined_imports['moving_import_rank'],
-        y=combined_imports['hs4'],
+        x=imports_data['moving_price_imports'],
+        y=imports_data['hs4'],
         orientation="h",
         textposition='auto',
         name="Top Imports"
