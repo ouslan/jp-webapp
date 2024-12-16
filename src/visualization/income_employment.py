b176184a117c5da769a19edec35adcdac8b8f804
Fernan2908 <fernando.sierra3@upr.edu>
2024-12-16 15:58:52 -0400

    Enhance income employment visualization by adding NAICS code filtering and updating HTML dropdown for NAICS selection

--- src/visualization/income_employment.py
+++ src/visualization/income_employment.py
@@ -5,4 +5,31 @@ import plotly.express as px
 import requests
 
 def web_app_income_employment(request):
-  return render(request, "income_employment.html")
+  df = pd.read_parquet("data/external/naics4_df.parquet")
+  df = pd.DataFrame(df)
+  
+  df_sorted = df.sort_values(by=["year", "qtr"], ascending=True)
+  df_sorted = df_sorted[df_sorted["year"] < 2024]
+  
+  
+  df_filtered = df_sorted[df_sorted["first_4_naics_code"] == "9999"]
+  df_filtered = df_filtered.sort_values(by=["year", "qtr"], ascending=True)
+  
+  x_axis = df_filtered.apply(lambda row: f"Q{row['qtr']} {row['year']}", axis=1)
+  
+  y_axis = df_filtered["total_employment_sum"]
+  
+  title = "Employment in the US by Year"
+  
+  naics = df_sorted.sort_values(by="first_4_naics_code")
+  naics = naics[naics["first_4_naics_code"] != "0"]["first_4_naics_code"].unique()
+
+  context = {
+    'naics_code': naics,
+  }
+  
+  fig = go.Figure()
+  fig.add_trace(go.Scatter(x=x_axis, y=y_axis, mode='lines+markers'))
+
+  fig.update_layout(title=title)
+  return render(request, "income_employment.html", {'graph': fig.to_html(), **context})
