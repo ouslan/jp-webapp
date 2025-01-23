5b081c2d6e76a5203e8879a89e3befba4744a343
Alejandro Ouslan <alejandro.ouslan@upr.edu>
2025-01-23 19:20:06 +0000

    98 prod (#121)
    * added request datathrough the api
    
    Signed-off-by: Alejandro Ouslan <alejandro.ouslan@upr.edu>
    
    * update the submodules
    
    * added nix and project toml #98
    
    * fixed misconfigured
    
    * update dependencies
    
    * added ci pipeline
    
    * update lock
    
    ---------
    
    Signed-off-by: Alejandro Ouslan <alejandro.ouslan@upr.edu>

--- env.py
+++ env.py
@@ -1,28 +1,32 @@
+from re import DEBUG
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
 # Get the Database credentials
-def get_db_credentials():
+def get_db_credentials() -> list:
     USER = str(os.environ.get("POSTGRES_USER")).strip()
     HOST = str(os.getenv("POSTGRES_HOST")).strip()
+    PORT = str(os.environ.get("POSTGRES_PORT")).strip()
     DATABASE = str(os.environ.get("POSTGRES_DB")).strip()
     SECRET_KEY = str(os.getenv("SECRET_KEY")).strip()
 
-    if not all([HOST, USER, DATABASE, SECRET_KEY]):
+    if not all([HOST, USER, DATABASE, SECRET_KEY, PORT]):
         raise ValueError("Database credentials not set")
     if os.environ.get("DEV") == "True":
         HOST = "localhost"
         PASSWORD = str(os.getenv("POSTGRES_PASSWORD")).strip()
-        DATABASE_URL = f"postgresql://{USER}:{PASSWORD}@{HOST}:5432/{DATABASE}"
+        DATABASE_URL = f"postgresql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}"
         API_URL = "api"
+        DEBUG = True
     else:
         HOST = "database"
         PASSWORD = str(read_secret_file('/run/secrets/db-password')).strip()
-        DATABASE_URL = f"postgresql://{USER}:{PASSWORD}@{HOST}:5432/{DATABASE}"
+        DATABASE_URL = f"postgresql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}"
         API_URL = "https://api.econlabs.net"
-    return [USER, PASSWORD, HOST, DATABASE, DATABASE_URL, SECRET_KEY, API_URL]
+        DEBUG = False
+    return [USER, PASSWORD, HOST, DATABASE, DATABASE_URL, SECRET_KEY, API_URL, PORT, DEBUG]
 
 def read_secret_file(secret_path):
     with open(secret_path, 'r') as file:
