ef35d4a0e4f40eafcf83bfdcba56cdbf6a9b0599
Fernando2908 <fernando.sierra3@upr.edu>
2025-01-23 11:06:56 -0400

    Enhance error handling in trade data fetching and processing; refactor frequency handling and update HTML options for consistency

--- src/visualization/product_hts.py
+++ src/visualization/product_hts.py
@@ -7,63 +7,92 @@ API_URL = "http://localhost:8057/"
 
 def fetch_trade_data(agg, hts_code):
     url = f"{API_URL}data/trade/jp/?&time&types=hts&agr=false&group=false&data_filter={hts_code}&agg={agg}&update=false"
-    response = requests.get(url).json()
-    return pd.DataFrame(response)
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  
+        return pd.DataFrame(response.json())
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching data: {e}")
+        return pd.DataFrame()  
+    except ValueError as e:
+        print(f"Error decoding JSON: {e}")
+        return pd.DataFrame()  
+    
+
+def fetch_hts_codes():
+        hts_codes_url = f"{API_URL}/data/trade/jp/hts_codes/"
+    
+        try:
+            response = requests.get(hts_codes_url)
+            response.raise_for_status()  
+            return pd.DataFrame(response.json())
+        except requests.exceptions.RequestException as e:
+            print(f"Error fetching data: {e}")
+            return pd.DataFrame()  
+        except ValueError as e:
+            print(f"Error decoding JSON: {e}")
+            return pd.DataFrame() 
+
+
 
 def sort_data(data, frequency, trade_type):
-    data = data.sort_values(by='year', ascending=True)
+    data = data.sort_values(by=frequency, ascending=True)
         
     data['hts_code_first2'] = data['hts_code'].str[:2]
-    data = data.sort_values(by=['year', 'hts_code_first2'], ascending=[True, True])
-    data = data.groupby(['year', 'hts_code_first2']).agg({trade_type: 'sum'}).reset_index()
+    data = data.sort_values(by=[frequency, 'hts_code_first2'], ascending=[True, True])
+    data = data.groupby([frequency, 'hts_code_first2']).agg({trade_type: 'sum'}).reset_index()
     
-    data = data[['year', 'hts_code_first2', trade_type]]
+    data = data[[frequency, 'hts_code_first2', trade_type]]
     
     return data
+
 
 def refactor_frequency(frequency):
     if frequency == 'yearly':
         return 'year'
     elif frequency == 'monthly':
         return 'month'
-    elif frequency == 'quarterly':
+    elif frequency == 'qrt':
         return 'qrt'
     elif frequency == 'fiscal':
         return 'fiscal'
+    
+def get_data_to_graph(frequency, hts_code, trade_type):
+    data = fetch_trade_data(frequency, hts_code)
+    new_frequency = refactor_frequency(frequency)
+    data = sort_data(data, new_frequency, trade_type)
+    
+    x_axis = data[new_frequency]
+    y_axis = data[trade_type]
+    
+    
+    hts_list = fetch_hts_codes()
+    print(hts_list)
+    
+    context = {
+        'hts_codes': hts_list['hts_code_first2'].tolist(),
+    }
+    
+    return x_axis, y_axis, context
 
 def products_hts(request):
     frequency = "yearly"
     hts_code = "01"
     trade_type = "imports"
     
-    data = fetch_trade_data(frequency, hts_code)
-    data = sort_data(data, frequency, trade_type)
+    x_axis, y_axis, context = get_data_to_graph(frequency, hts_code, trade_type)
     
-    x_axis = data['year']
-    y_axis = data[trade_type]
-    context = {
-        'frequency': frequency,
-        'hts_code': hts_code,
-        'trade_type': trade_type
-    }
+    print(frequency, hts_code, trade_type)
 
     if request.method == 'POST':
         frequency = request.POST.get('frequency')
         hts_code = request.POST.get('hts_code')
         trade_type = request.POST.get('trade_type')
         
-        data = fetch_trade_data(frequency, hts_code)
-        new_frequency = refactor_frequency(frequency)
-        data = sort_data(data, new_frequency)
+        print(frequency, hts_code, trade_type)
+        x_axis, y_axis, context = get_data_to_graph(frequency, hts_code, trade_type)
         
-        x_axis = data[new_frequency]
-        y_axis = data[trade_type]
         
-        context = {
-            'frequency': frequency,
-            'hts_code': hts_code,
-            'trade_type': trade_type
-        }
             
     # Add title to the graph
     title = f"Frequency: {frequency}    HTS Code: {hts_code}    Trade Type: {trade_type}"
@@ -75,5 +104,5 @@ def products_hts(request):
     fig.update_layout(title=title)
 
     # Render the template with the graph
-    return render(request, 'product_hts.html', {'graph': fig.to_html(), 'context': context})
+    return render(request, 'product_hts.html', {'graph': fig.to_html(), **context})
     
