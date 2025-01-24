c2e4394917405916d8fdca92f7318c471c39e86c
Alejandro Ouslan <alejandro.ouslan@upr.edu>
2025-01-24 09:23:42 -0400

    added option for debug

--- env.py
+++ env.py
@@ -1,4 +1,3 @@
-from re import DEBUG
 from dotenv import load_dotenv
 import os
 
@@ -12,6 +11,7 @@ def get_db_credentials() -> list:
     PORT = str(os.environ.get("POSTGRES_PORT")).strip()
     DATABASE = str(os.environ.get("POSTGRES_DB")).strip()
     SECRET_KEY = str(os.getenv("SECRET_KEY")).strip()
+    DEBUG = os.getenv("DEBUG")
 
     if not all([HOST, USER, DATABASE, SECRET_KEY, PORT]):
         raise ValueError("Database credentials not set")
@@ -20,14 +20,12 @@ def get_db_credentials() -> list:
         PASSWORD = str(os.getenv("POSTGRES_PASSWORD")).strip()
         DATABASE_URL = f"postgresql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}"
         API_URL = "api"
-        DEBUG = True
     else:
         HOST = "database"
         PORT = "5432"
         PASSWORD = str(read_secret_file("/run/secrets/db-password")).strip()
         DATABASE_URL = f"postgresql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}"
         API_URL = "https://api.econlabs.net"
-        DEBUG = False
     return [
         USER,
         PASSWORD,
@@ -40,7 +38,7 @@ def get_db_credentials() -> list:
         DEBUG,
     ]
 
+
 def read_secret_file(secret_path):
     with open(secret_path, "r") as file:
-
         return file.read().strip()
