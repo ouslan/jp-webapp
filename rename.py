0bcd27e21b1fc8b1de14073043a878fc3877faf4
Alejandro Ouslan <alejandro.ouslan@upr.edu>
2025-01-27 11:55:36 -0400

    change base to use react

--- rename.py
+++
@@ -1,6 +0,0 @@
-import os
-
-for directory in os.listdir("data/indicadores_economicos"):
-    for file in os.listdir(f"data/indicadores_economicos/{directory}"):
-        os.rename(f"data/indicadores_economicos/{directory}/{file}", f"data/indicadores_economicos/{directory}/{file.lower().replace(" ", "_")}")
-
--- web_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-class WebAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'web_app'
