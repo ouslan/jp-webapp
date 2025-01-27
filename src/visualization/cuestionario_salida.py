cf4b63eccc9ed824390b289f08c1d5671e834e4a
Alejandro Ouslan <alejandro.ouslan@upr.edu>
2025-01-27 11:51:06 -0400

    added react

--- src/visualization/cuestionario_salida.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from src.dao.data_db_dao import DAO
-import polars as pl
-
-def cuestionario_viajero_salida(request):
-  return render(request, 'cuestionario_salida.html')
