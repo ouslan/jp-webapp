1a68bafeb0bf5083eceebbc7ce687437a3d3a611
Fernando2908 <fernando.sierra3@upr.edu>
2025-02-12 11:41:08 -0400

    add blank line for improved code readability in JP-362 quarterly form

--- src/Forms/quaterly/balanza_de_pagos/JP-362-qtr.tsx
+++ src/Forms/quaterly/balanza_de_pagos/JP-362-qtr.tsx
@@ -14,6 +14,7 @@ function JP_362_qrt() {
                 <br />
                 Rev. 7/17
                 <br />
+                
                 <input
                   id="header_inputs_right"
                   type="number"
