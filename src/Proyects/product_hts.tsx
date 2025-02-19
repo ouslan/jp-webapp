6eb607b28210d45e45811c20118bd63de2e1c479
Fernando2908 <fernando.sierra3@upr.edu>
2025-02-19 12:40:48 -0400

    Refactor footer structure in ProductHTS component for improved layout

--- src/Proyects/product_hts.tsx
+++ src/Proyects/product_hts.tsx
@@ -98,52 +98,50 @@ const ProductHTS: React.FC = () => {
             />
           </div>
         </form>
+        <footer>
+            <section>
+              <h1>Contáctanos</h1>
+            </section>
+            <section>
+              <div className="footer-container">
+                <div className="left-container">
+                  <h2>Oficiales de Información <br /> (Ley 141-2019)</h2>
+                  <p>
+                    Sr. Edgardo Vázquez Rivera <br />
+                    Secretario Oficina de Secretaría <br />
+                    <a href="mailto:Vazquez_e@jp.pr.gov" className="custom-link">Vazquez_e@jp.pr.gov</a> <br />
+                    787 723-6200 ext. 16637 <br /><br />
+                    Plan. Erika Rivera Felicié <br />
+                    Ayudante Especial <br />
+                    <a href="mailto:ivera_e1@jp.pr.gov" className="custom-link">ivera_e1@jp.pr.gov</a> <br /><br />
+                    Lcda Aida Silver Cintrón <br />
+                    Abogada Oficina Asuntos Legales <br />
+                    <a href="mailto:Silver_A@jp.pr.gov" className="custom-link">Silver_A@jp.pr.gov</a> <br />
+                    787 723-6200 ext. 16016
+                  </p>
+                </div>
+                <div className="right-container">
+                  <h2><strong>Dirección Postal</strong></h2>
+                  <p>
+                    PO Box 4119 <br />
+                    San Juan P.R. 00940-1119 <br />
+                    Tel: (787) 723-6200 (Cuadro)
+                  </p>
+                  <h2><strong>Dirección Física</strong></h2>
+                  <p>
+                    Centro Gubernamental<br />
+                    Roberto Sánchez Vilella<br />
+                    Ave. De Diego, Pda 22, <br />
+                    Santurce
+                  </p>
+                  <section className="maps">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3784.7501887340845!2d-66.0698374248245!3d18.449648171336893!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8c036f497801a7b7%3A0x68ace32a0663b72f!2sGovernor%20Roberto%20S%C3%A1nchez%20Vilella%20Government%20Center!5e0!3m2!1sen!2sus!4v1717000266481!5m2!1sen!2sus" width="490" height="200" style={{ border: 0 }} allowFullScreen loading="lazy"></iframe>
+                  </section>
+                </div>
+              </div>
+            </section>
+        </footer>
       </main>
-      <footer>
-        <main>
-          <section>
-            <h1>Contáctanos</h1>
-          </section>
-          <section>
-            <div className="footer-container">
-              <div className="left-container">
-                <h2>Oficiales de Información <br /> (Ley 141-2019)</h2>
-                <p>
-                  Sr. Edgardo Vázquez Rivera <br />
-                  Secretario Oficina de Secretaría <br />
-                  <a href="mailto:Vazquez_e@jp.pr.gov" className="custom-link">Vazquez_e@jp.pr.gov</a> <br />
-                  787 723-6200 ext. 16637 <br /><br />
-                  Plan. Erika Rivera Felicié <br />
-                  Ayudante Especial <br />
-                  <a href="mailto:ivera_e1@jp.pr.gov" className="custom-link">ivera_e1@jp.pr.gov</a> <br /><br />
-                  Lcda Aida Silver Cintrón <br />
-                  Abogada Oficina Asuntos Legales <br />
-                  <a href="mailto:Silver_A@jp.pr.gov" className="custom-link">Silver_A@jp.pr.gov</a> <br />
-                  787 723-6200 ext. 16016
-                </p>
-              </div>
-              <div className="right-container">
-                <h2><strong>Dirección Postal</strong></h2>
-                <p>
-                  PO Box 4119 <br />
-                  San Juan P.R. 00940-1119 <br />
-                  Tel: (787) 723-6200 (Cuadro)
-                </p>
-                <h2><strong>Dirección Física</strong></h2>
-                <p>
-                  Centro Gubernamental<br />
-                  Roberto Sánchez Vilella<br />
-                  Ave. De Diego, Pda 22, <br />
-                  Santurce
-                </p>
-                <section className="maps">
-                  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3784.7501887340845!2d-66.0698374248245!3d18.449648171336893!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8c036f497801a7b7%3A0x68ace32a0663b72f!2sGovernor%20Roberto%20S%C3%A1nchez%20Vilella%20Government%20Center!5e0!3m2!1sen!2sus!4v1717000266481!5m2!1sen!2sus" width="490" height="200" style={{ border: 0 }} allowFullScreen loading="lazy"></iframe>
-                </section>
-              </div>
-            </div>
-          </section>
-        </main>
-      </footer>
     </div>
   );
 };
